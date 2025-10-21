dbccc5ef59afe81707fc0f673b1da1f175954bd0
rumesh <rumeshk066@gmail.com>
2025-10-21 11:36:06 +0530

    update .env for MongoDB connection and add initial server setup in index.js

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+// const authRoutes = require('./routes/authRoutes');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB first
+connectDB();
+
+// Middleware
+// app.use(cors());
+// app.use(express.json());
+
+// // Routes
+// app.use('/api/auth', authRoutes);
+
+// Start server after DB connection
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
